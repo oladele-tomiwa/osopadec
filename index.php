6a763390ae229865d9cfb4c9e1e386e6f4fa7bc0
ladhenrytom <80552159+ladhenrytom@users.noreply.github.com>
2021-03-13 00:08:57 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home page: Ondo State Oil Producing Area Development Commission</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap-4.2.1-dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script type="text/javascript" src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="bootstrap-4.2.1-dist/js/bootstrap.js"></script>
+</head>
+<body>
+
+	<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+		<div class="container-fluid">
+			<div class="row col-md-1">
+				<a class="navbar-brand" href=""><img src="pishurs/osopadec.png" width="100%" height="100%"></a>
+			</div>
+
+			<div class="row col-md-8">
+				<a class="navbar-brand" href=""><h4>Ondo State Oil Producing Area Development Commission</h4></a>
+			</div>
+
+			<script type="text/javascript">
+				function clickButton() {
+				document.getElementById('navbarResponsive').classList.toggle("show");
+				}
+			</script>
+
+			<button onClick="clickButton()" class="navbar-toggler" type="button" data-toggler="collapse" data-target="#navbarResponsive">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarResponsive">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="">About</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="">Services</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="">Team</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="">Support</a>
+					</li>
+				</ul>
+			</div>
+			
+		</div>
+	</nav>
+
+	<div id="slides" class="carousel slide" data-ride=carousel>
+		<ul class="carousel-indicators">
+			<li data-target="#slides" data-slide-to="0" class="active"></li>
+			<li data-target="#slides" data-slide-to="1"></li>
+			<li data-target="#slides" data-slide-to="2"></li>
+		</ul>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img src="pishurs/me.jpg">
+				<div class="carousel-caption">
+					<h1 class="display-4">DATABASE UPDATE</h1>
+					<h3>Me taking a photo so as to be included in the database</h3>
+					<button type="button" class="btn btn-outline-light btn-lg">View Demo</button>
+					<button type="button" class="btn btn-primary btn-lg">Get Started</button>
+				</div>
+			</div>
+			<div class="carousel-item">
+				<img src="pishurs/team1.jpg">
+			</div>
+			<div class="carousel-item">
+				<img src="pishurs/team2.jpg">
+			</div>
+		</div>
+	</div>
+	
+	<div class="container-fluid">
+		<div class="row jumbotron">
+			<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10">
+				<p class="lead"> A Web hosting service consists having a person, particularly, a developer, manage your website for you. Fortunately, we offer such services here at the ICT unit. Click the link to learn more.</p>
+			</div>
+			<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2">
+				<a href=""><button type="button" class="btn btn-outline-secondary btn-lg">Web Hosting</button></a>
+			</div>
+			
+		</div>
+		
+	</div>
+	
+	<div class="container-fluid padding">
+		<div class="row welcome text-center">
+			<div class="col-12">
+				<h1 class="display-4">Developed by Mr. Oladele Tomiwa.</h1><br>
+			</div>
+			<hr>
+			<div class="col-12">
+				<p> Welcome to the website experience of my industrial training. It might be boring but please, bear with me as this is experimental. God bless you as you go through.</p><p>Welcome to the website experience of my industrial training. It might be boring but please, bear with me as this is experimental. God bless you as you go through.</p><p>Welcome to the website experience of my industrial training. It might be boring but please, bear with me as this is experimental. God bless you as you go through.</p><br>
+			</div>
+		</div>
+	</div>
+	
+	<div class="container-fluid padding">
+		<div class="row text-center">
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<i class="fas fa-code"></i>
+				<h3>HTML 5</h3>
+				<p>built with the latest version of HTML, HTML5.</p>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<i class="fas fa-bold"></i>
+				<h3>Bootstrap</h3>
+				<p>built with the latest version of bootstrap, BOOTSTRAP 4.</p>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<i class="fab fa-css3"></i>
+				<h3>CSS3</h3>
+				<p>built with the latest version of CSS, CSS3.</p>
+			</div>
+		</div>
+		<hr class="my-4">
+	</div>
+	
+	<div class="container-fluid padding">
+		<div class="row padding">
+			<div class="col-md-12 col-lg-6">
+				<h2>The ICT unit...</h2>
+				<p>A Web hosting service consists having a person, particularly, a developer, manage your website for you. A Web hosting service consists having a person, particularly, a developer, manage your website for you.</p>
+				<p>A Web hosting service consists having a person, particularly, a developer, manage your website for you.</p>
+			    <p>A Web hosting service consists having a person, particularly, a developer, manage your website for you</p>
+			    <br>
+			    <a href="" class="btn btn-primary">learn more</a>
+			</div>
+
+			<div class="col-lg-6">
+				<img src="pishurs/mephotoshop2.jpg" class="img-fluid">
+			</div>
+		</div>
+	</div>
+	
+	<hr class="my-4">
+	
+	<figure>
+		<div class="fixed-wrap">
+			<div id="fixed"></div>
+		</div>
+	</figure>
+	
+	<button class="fun" data-toggle="collapse" data-target="#emoji">click for fun</button>
+	<div id="emoji" class="collapse">
+		<div class="container-fluid padding">
+			<div class="row text-center">
+				<div class="col-sm-6 col-md-3">
+					<img src="pishurs/osopadec.png" class="gif">
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<img src="pishurs/osopadec.png" class="gif">
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<img src="pishurs/osopadec.png" class="gif">
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<img src="pishurs/osopadec.png" class="gif">
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="container-fluid padding">
+		<div class="row welcome text-center">
+			<div class="col-12">
+				<h1 class="display-4">Meet The ICT Unit Team</h1>
+			</div>
+			<br><hr>
+		</div>
+	</div>
+	
+	<div class="container-fluid padding">
+		<div class="row padding">
+			<div class="col-md-4">
+				<div class="card">
+					<img src="pishurs/team1.jpg" class="card-img-top">
+					<div class="card-body">
+						<h4 class="card-title">Lawal Quazeem</h4>
+						<p class="card-text">Lawal Quazeem is an IT student from the Federal University Of Technology, Minna.</p>
+						<a href="" class="btn btn-outline-secondary">see profile</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card">
+					<img src="pishurs/team2.jpg" class="card-img-top">
+					<div class="card-body">
+						<h4 class="card-title">Oladele Tomiwa</h4>
+						<p class="card-text">Oladele Tomiwa is an IT student from the Federal University Of Technology, Akure.</p>
+						<a href="" class="btn btn-outline-secondary">see profile</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card">
+					<img src="pishurs/team3.jpg" class="card-img-top">
+					<div class="card-body">
+						<h4 class="card-title">John Osafisan</h4>
+						<p class="card-text">Mr John Osafisan is an employee with more than 10 years experience.</p>
+						<a href="" class="btn btn-outline-secondary">see profile</a><br>
+					</div><br>
+				</div>
+			</div>
+		</div>
+	</div><br><br>
+	
+	<div class="container-fluid padding">
+		<div class="row padding">
+			<div class="col-md-12 col-lg-6">
+				<h2>Our goals...</h2>
+				<p>A Web hosting service consists having a person, particularly, a developer, manage your website for you. A Web hosting service consists having a person, particularly, a developer, manage your website for you.</p>
+				<p>A Web hosting service consists having a person, particularly, a developer, manage your website for you.</p>
+			    <p>A Web hosting service consists having a person, particularly, a developer, manage your website for you</p>
+			    <br>
+			</div>
+
+			<div class="col-lg-6">
+				<img src="pishurs/goals.jpg" class="img-fluid"><br>
+			</div><br>
+		</div><br>
+		<hr class="my-4">
+	</div>
+	
+	<div class="container-fluid padding">
+		<div class="row text-center padding">
+			<div class="col-12">
+				<h2><strong>connect</strong></h2>
+			</div>
+			<div class="col-12 social padding">
+				<a href="facebook.com"><i class="fab fa-facebook"></i></a>
+				<a href="twitter.com"><i class="fab fa-twitter"></i></a>
+				<a href="instagram.com"><i class="fab fa-instagram"></i></a>
+				<a href="googlemail.com"><i class="fab fa-google-plus-g"></i></a>
+				<a href="youtube.com"><i class="fab fa-youtube"></i></a><br>
+			</div><br>
+		</div><br>
+	</div>
+	
+	<footer>
+		<div class="container-fluid padding">
+			<div class="row text-center">
+				<div class="col-md-4">
+					<hr class="light">
+					<h2>contact us</h2>
+					<hr class="light">
+					<p>+234-809-988-3821</p>
+					<p>email@osopadec.info.ng</p>
+					<p>PMB 704</p>
+					<p>Oba-Ile, Nigeria.</p>
+				</div>
+				<div class="col-md-4">
+					<hr class="light">
+					<h2>Our Hours</h2>
+					<hr class="light">
+					<p>Monday - Friday</p>
+					<p>8am - 4pm</p>
+					<p>Closed: Saturday and Sunday.</p>
+				</div>
+				<div class="col-md-4">
+					<hr class="light">
+					<p><h2>Service Locations</h2></p>
+					<hr class="light">
+					<p>Akure, Ondo State.</p>
+					<p>Ilaje, Ondo State.</p>
+					<p>Ese-Odo, Ondo State.</p>
+				</div>
+				<div class="col-12">
+					<hr class="light-100">
+					<h6>&copy; @osopadec.com.ng</h6>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
